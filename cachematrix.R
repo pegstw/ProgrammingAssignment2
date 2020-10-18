dff1934ab72770177a4604e54282925f7c7c1dd9
pegstw <pegstw@users.noreply.github.com>
2020-10-18 18:09:44 +0800

    Update cachematrix.R
    coursera submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##makeCacheMatrix sets and gets the matrix first 
+##later sets and gets the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invmat <- NULL
+  set <- function(y) {
+    x <<- y
+    invmat <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) invmat <<- inverse
+  getinv <- function() {invmat}
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+##cacheSolve checks to see whether the inv has been computed
+##if yes provides cached solution else solves for matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invmat <- x$getinv()
+  if(!is.null(invmat)) {
+    message("getting cached data")
+  return(invmat)
+    }
+  matx <- x$get()
+  invmat <- solve(matx, ...)
+  x$setinv(invmat)
+  invmat
 }
+
